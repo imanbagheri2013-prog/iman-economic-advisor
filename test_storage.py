18cb07d3a524b485615c8965b09f911bb8058674
imanbagheri2013-prog <imanbagheri2013@gmail.com>
2026-08-28 05:36:15 -0700

    Delete test_storage.py

--- test_storage.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime,timezone
-from iea.models import Observation
-from iea.storage import Store
-def test_store(tmp_path):
-    s=Store(str(tmp_path/'x.sqlite3'))
-    s.upsert(Observation(provider='t',series_id='x',
-      date=datetime(2026,1,1,tzinfo=timezone.utc),value=1,
-      retrieved_at=datetime.now(timezone.utc),quality=100))
-    assert s.count()==1
