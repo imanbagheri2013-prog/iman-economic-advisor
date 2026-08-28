b09e6b111986a6b541256649c9ea70bea9b4e88d
imanbagheri2013-prog <imanbagheri2013@gmail.com>
2026-08-28 05:41:58 -0700

    Delete test_quality.py

--- test_quality.py
+++
@@ -1,4 +0,0 @@
-from datetime import datetime,timezone
-from iea.quality import score
-def test_score(): assert score(1,datetime.now(timezone.utc))==100
-def test_missing(): assert score(None,datetime.now(timezone.utc))==0
